f6f3bdc25f77877f59c28e228e50e9ab4183309c
ticknotty <nicktotty8@gmail.com>
2015-11-24 12:51:20 +0000

    Changed js code

--- server.js
+++ server.js
@@ -5,7 +5,20 @@
 var http = require('http')
 var port = process.env.PORT || 1337;
 
-http.createServer(function(request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('Hello World\n');
-}).listen(port);
+var server = http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("<!DOCTYPE 'html'>");
+    response.write("<html>");
+    response.write("<head>");
+    response.write("<title>Hello World Page</title>");
+    response.write("</head>");
+    response.write("<body>");
+    response.write("<h1>");
+    response.write("Hello World!");
+    response.write("</h1>");
+    response.write("</body>");
+    response.write("</html>");
+    response.end();
+});
+server.listen(port);
+console.log("Server is listening");
