5eaa97a70a9c77fbb0a05b75fc0d866bb082ca20
ticknotty <nicktotty8@gmail.com>
2015-11-24 12:45:57 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+/**
+ * Created by 1202663 on 24/11/2015.
+ */
+
+var http = require('http')
+var port = process.env.PORT || 1337;
+
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hello World\n');
+}).listen(port);
