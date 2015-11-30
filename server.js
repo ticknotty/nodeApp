0c56a4d62f10fe5097328f30dcd596e6d6c4ecdf
ticknotty <nicktotty8@gmail.com>
2015-11-30 17:43:18 +0000

    changed server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var client = new Twitter({
 });
 
 http.createServer(function(request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin' : '*' });
 
     client.get('search/tweets', {q: 'lolcats'}, function(error, tweets){
         response.end(JSON.stringify(tweets));
