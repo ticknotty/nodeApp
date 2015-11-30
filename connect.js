0469a717b446ed6952821647b4f4c706fd45ddf0
ticknotty <nicktotty8@gmail.com>
2015-11-30 13:15:29 +0000

    tried different username

--- connect.js
+++ connect.js
@@ -9,6 +9,6 @@ var http = require('http');
 //setup our port
 var port = process.env.PORT || 1337;
 // Connection URL. This is where your mongodb server is running.
-var url = 'mongodb://nicktotty:moffat51@ds054308.mongolab.com:54308/nodeappdb'
+var url = 'mongodb://ticknotty:moffat51@ds054308.mongolab.com:54308/nodeappdb'
 // We need to work with "MongoClient" interface in order to connect to a mongodb server.
 var MongoClient = mongodb.MongoClient;
