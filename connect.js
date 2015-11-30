a2bb21e1b983c299fad67dab358aea62bdd8e4ee
ticknotty <nicktotty8@gmail.com>
2015-11-30 16:51:35 +0000

    channged server.js

--- connect.js
+++ connect.js
@@ -13,4 +13,27 @@ var url = 'mongodb://nicktotty:moffat51@ds054288.mongolab.com:54288/rgutest';
 // We need to work with "MongoClient" interface in order to connect to a mongodb server.
 var MongoClient = mongodb.MongoClient;
 
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write('Connecting \n');
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function (err, db) {
+        response.write('Connection Made \n');
+        if (err) {
+            response.write('Unable to connect to the mongoDB server. Error:' + err + "\n");
+            //Error so close connection
+            db.close();
+        } else {
+            //HURRAY!! We are connected. :)
+            response.write('Connection established to' + url +"\n");
+
+            // do some work here with the database.
+
+            //Done Close connection
+            db.close();
+        }
+        response.end('Finished, Connection closed \n');
+    });
+
+}).listen(port);
 
