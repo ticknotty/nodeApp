50c6decba39afcc51c1553c0622ad6a9af23fb1a
ticknotty <nicktotty8@gmail.com>
2015-11-30 17:02:30 +0000

    changed connect to retrieve in server.js

--- retrieve.js
+++ retrieve.js
@@ -48,9 +48,7 @@ http.createServer(function(request, response) {
 
 
             //Done Close connection
-            db.close();
         }
-        response.end('Finished, Connection closed \n');
     });
 
 }).listen(port);
